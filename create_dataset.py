6b03bcb44d16a71860b00f920fed9912ebce9854
gentry <gma23@txstate.edu>
2021-04-06 16:42:41 -0500

    made a little file to view some data

--- create_dataset.py
+++ create_dataset.py
@@ -17,21 +17,21 @@ Maybe I'll normalize the data before writing it.
 '''
 
 if __name__ == "__main__":
-    print('Preparing 5000 instances of 500 to 1500 seconds (10 to 30 seconds at 50Hz)')
+    print('Preparing 5000 instances of 500 to 1000 seconds (10 to 20 seconds at 50Hz)')
     classes = [
         {
-            'avg_pattern_length':randint(3, 7),
-            'avg_amplitude':randint(1, 3),
-            'default_variance':randint(1, 3),
-            'variance_pattern_length': randint(5, 20),
-            'variance_amplitude': randint(1, 5)
+            'avg_pattern_length':3,
+            'avg_amplitude':1,
+            'default_variance':1,
+            'variance_pattern_length': 10,
+            'variance_amplitude': 2
         },
         {
-            'avg_pattern_length':randint(3, 7),
-            'avg_amplitude':randint(1, 3),
-            'default_variance':randint(1, 3),
-            'variance_pattern_length': randint(5, 20),
-            'variance_amplitude': randint(1, 5)
+            'avg_pattern_length':7,
+            'avg_amplitude':3,
+            'default_variance':3,
+            'variance_pattern_length': 15,
+            'variance_amplitude': 4
         },
     ]
     print('Preparing data with ', len(classes), ' classes.')
@@ -39,27 +39,29 @@ if __name__ == "__main__":
     for i in range(2500):
         instances.append(
             {
-                'instance number': i+1,
+                'instance_number': i+1,
                 'samples': generate_pattern_data_as_array(
-                    length=randint(500, 1500),
+                    length=randint(500, 1000),
                     avg_pattern_length=classes[0]['avg_pattern_length'],
                     default_variance=classes[0]['default_variance'],
                     variance_pattern_length=classes[0]['variance_pattern_length'],
-                    variance_amplitude=classes[0]['variance_amplitude']
-                )
+                    variance_amplitude=classes[0]['variance_amplitude'],
+                ),
+                'label':int(0)
             }
         )
     for i in range(2500):
         instances.append(
             {
-                'instance number': 2500+i+1,
+                'instance_number': 2500+i+1,
                 'samples': generate_pattern_data_as_array(
-                    length=randint(500, 1500),
+                    length=randint(500, 1000),
                     avg_pattern_length=classes[1]['avg_pattern_length'],
                     default_variance=classes[1]['default_variance'],
                     variance_pattern_length=classes[1]['variance_pattern_length'],
                     variance_amplitude=classes[1]['variance_amplitude']
-                )
+                ),
+                'label':int(1)
             }
         )
     instance_frame = pandas.DataFrame(instances)
@@ -68,5 +70,5 @@ if __name__ == "__main__":
     print('Keys: ', instances[0].keys())
     print('Shortest instance: ', min([len(i['samples']) for i in instances]))
     print('Longest instance: ', max([len(i['samples']) for i in instances]))
-    instance_frame.to_csv('pip_test_data.csv')
+    instance_frame.to_csv('pip_test_data.csv', columns=['instance_number', 'samples', 'label'], sep=',', index=False)
     print('done')
---
+++ initial_visualization.py
@@ -0,0 +1,55 @@
+#Author: Gentry Atkinson
+#Organization: Texas University
+#Data: 6 April, 2021
+#Make some line graphs of our test data
+import pandas
+from matplotlib import pyplot as plt
+from utils.ts_feature_toolkit import get_features_from_one_signal
+import numpy as np
+from sklearn.manifold import TSNE as tsne
+
+if __name__ == "__main__":
+    instances = pandas.read_csv('pip_test_data.csv')
+    print('Keys: ',  instances.keys())
+    print('D types: ', instances.dtypes)
+    print('Number of instances: ', len(instances['samples']))
+
+    plt.figure(1)
+    plt.title("4 Instances of Each Class")
+    plt.subplot(211)
+    for s in instances['samples'][:4]:
+        s = s[1:-2]
+        s = s.replace('\n', '')
+        s = s.replace('[', '')
+        s = s.replace(']', '')
+        s = [float(x) for x in s.split(' ') if x]
+        # print(len(s))
+        plt.plot(s, c='blue')
+    plt.subplot(212)
+    for s in instances['samples'][2500:2504]:
+        s = s[1:-2]
+        s = s.replace('\n', '')
+        s = s.replace('[', '')
+        s = s.replace(']', '')
+        s = [float(x) for x in s.split(' ') if x]
+        # print(len(s))
+        plt.plot(s, c='red')
+    plt.savefig('pip_test_data_raw_plot.pdf')
+    #plt.show()
+
+    features = []
+    for s in instances['samples'][:]:
+        s = s[1:-2]
+        s = s.replace('\n', '')
+        s = s.replace('[', '')
+        s = s.replace(']', '')
+        s = [float(x) for x in s.split(' ') if x]
+        features.append(get_features_from_one_signal(np.array(s), 50))
+
+    plt.figure(2)
+    vis = tsne(n_components=2, n_jobs=8).fit_transform(features)
+    for v in vis[0:2500]:
+        plt.scatter(v[0], v[1], c='blue', marker=".")
+    for v in vis[2500:5000]:
+        plt.scatter(v[0], v[1], c='red', marker=".")
+    plt.savefig('pip_test_data_tsne.pdf')
